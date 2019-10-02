ec0c7093af41de4ddc0d71c424b1d1fa69aef614
robert-barcelona <robertanthony.bcn@gmail.com>
2019-10-02 18:28:18 +0200

    pie chart

--- src/components/breedchart/BreedChart.js
+++ src/components/breedchart/BreedChart.js
@@ -1,20 +1,18 @@
 import React from "react"
 import { useSelector } from "react-redux"
-import randomColor from 'randomcolor'
-
-import PieChart from "react-minimal-pie-chart"
+import { VictoryPie } from "victory"
 
 const BreedChart = () => {
   const chartData = useSelector(state => state.chartData)
 
   const pieData = chartData.map(datum => {
-    const [name,images] = datum
-    return ({title:name,value:images,color:randomColor()})
+    const [name, images] = datum
+    return { label: name, x: images }
   })
 
   return (
     <div>
-      {pieData && pieData.length !== 0 && <PieChart data={pieData} label={labelProps => labelProps.data.title} />}
+      {pieData && pieData.length !== 0 && <VictoryPie data={pieData} />}
     </div>
   )
 }
